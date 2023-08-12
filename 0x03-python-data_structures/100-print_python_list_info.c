32f3d903824118793f6771c80d7cf2fc50bfc4af
MOHAMED <lal939877@gmail.com>
2023-08-12 11:14:17 +0000

    a

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -2,6 +2,7 @@
 
 /**
  * print_python_list_info - Prints basic info about Python lists.
+ *
  * @p: A PyObject list.
  */
 void print_python_list_info(PyObject *p)
@@ -23,4 +24,3 @@ void print_python_list_info(PyObject *p)
 		printf("%s\n", Py_TYPE(obj)->tp_name);
 	}
 }
-
